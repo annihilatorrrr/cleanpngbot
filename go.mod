1f785ed18d40516d8c3a1215fcc24575a2ae8d21
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-22 19:47:59 +0530

    Done. (#1)

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
-module github.com/annihilatorrrr/gotemplate
+module cleanpngbot
 
 go 1.19
 
-require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.10
+require (
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.10
+	github.com/anaskhan96/soup v1.2.5
+)
+
+require (
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,83 @@
 package main
 
 import (
-	"log"
-	"os"
-	"runtime"
-	"sync"
-	"time"
-
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
-
+	"fmt"
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
+	"github.com/anaskhan96/soup"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
 )
+
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word to search in cleanpng.com!\nBy @Memers_Gallery!\nSourceCode: https://github.com/annihilatorrrr/cleanpngbot",
+		&gotgbot.SendMessageOpts{DisableWebPagePreview: true})
+	return ext.EndGroups
+}
+
+func sendres(b *gotgbot.Bot, ctx *ext.Context) error {
+	msg := ctx.EffectiveMessage
+	if msg.Text == "" {
+		return ext.EndGroups
+	}
+	if len(msg.Text) > 50 {
+		_, _ = msg.Reply(b, "Query too big!", nil)
+		return ext.EndGroups
+	}
+	em, err := msg.Reply(b, "Finding ...", nil)
+	if err != nil {
+		return ext.EndGroups
+	}
+	query := strings.ToLower(msg.Text)
+	if strings.Contains(query, " ") {
+		query = strings.Join(strings.Split(query, " "), "-")
+	}
+	raw, err := soup.Get(fmt.Sprintf("https://www.cleanpng.com/free/%s.html", query))
+	if err != nil {
+		_, _, _ = em.EditText(b, err.Error(), nil)
+		return ext.EndGroups
+	}
+	datas := soup.HTMLParse(raw).FindAll("article")
+	aa := false
+	txt := fmt.Sprintf("<b>Here's the search results for %s with thier resolution and disk size:</b>\n\n", query)
+	for _, rdata := range datas {
+		aa = true
+		pd := rdata.FindAll("p")
+		txt += fmt.Sprintf(`<b>> <a href="https://www.cleanpng.com%s">%s</a> - %s - %s</b>`+"\n",
+			rdata.Find("a").Attrs()["href"],
+			pd[0].Find("a").Text(),
+			pd[1].Find("span").Text(),
+			pd[2].Find("span").Text(),
+		)
+	}
+	txt += "\n<b>@Memers_Gallery</b>"
+	if !aa {
+		txt = "No data Found!\n<b>@Memers_Gallery</b>"
+	}
+	_, _, err = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{DisableWebPagePreview: true, ParseMode: "html"})
+	if err != nil {
+		_, _, _ = em.EditText(b, err.Error(), nil)
+	}
+	return ext.EndGroups
+}
 
 func main() {
 	token := os.Getenv("TOKEN")
 	if token == "" {
-		token = "111:3333kkkk"
+		panic("No bot token was found!")
+	}
+	url := os.Getenv("URL")
+	if url == "" {
+		panic("No webhook url was found!")
+	}
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	if port == 0 {
+		panic("No port was found to bind!")
 	}
 	b, err := gotgbot.NewBot(token, nil)
 	if err != nil {
@@ -27,24 +87,31 @@ func main() {
 		ErrorLog: nil,
 		DispatcherOpts: ext.DispatcherOpts{
 			Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
-				log.Println("An error occurred while handling update:", err.Error())
+				log.Printf("An error occurred while handling update:\n%s", err.Error())
 				return ext.DispatcherActionNoop
 			},
 			MaxRoutines: 20,
 		},
 	})
-	err = updater.StartPolling(b, &ext.PollingOpts{
+	_, err = b.SetWebhook(url, &gotgbot.SetWebhookOpts{
 		DropPendingUpdates: true,
-		GetUpdatesOpts: gotgbot.GetUpdatesOpts{
-			Timeout: 5,
-			RequestOpts: &gotgbot.RequestOpts{
-				Timeout: time.Second * 5,
-			},
-		},
+		AllowedUpdates:     []string{"message"},
+		MaxConnections:     20,
+		SecretToken:        "xyzzz",
 	})
 	if err != nil {
 		panic(err.Error())
 	}
+	err = updater.StartWebhook(b,
+		ext.WebhookOpts{
+			Port: port,
+		})
+	if err != nil {
+		panic(err.Error())
+	}
+	dispatcher := updater.Dispatcher
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewMessage(message.ChatType("private"), sendres))
 	log.Printf("%s has been started!\n", b.User.Username)
 	runtime.GC()
 	updater.Idle()
