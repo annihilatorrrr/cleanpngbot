80b591da5c4c1fcc4996bfeb8a8855d309c9edb1
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-22 20:44:43 +0530

    Update main.go
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 func start(b *gotgbot.Bot, ctx *ext.Context) error {
-	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word to search in cleanpng.com!\nBy @Memers_Gallery!\nSourceCode: https://github.com/annihilatorrrr/cleanpngbot",
+	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word to search in cleanpng.com!\nBy @Memers_Gallery!\nSource code: https://github.com/annihilatorrrr/cleanpngbot",
 		&gotgbot.SendMessageOpts{DisableWebPagePreview: true})
 	return ext.EndGroups
 }
