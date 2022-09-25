546e7522812b26bcad9fe55e8430d577f93ba782
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-25 17:11:15 +0530

    WIP!

--- main.go
+++ main.go
@@ -19,7 +19,14 @@ import (
 
 func start(b *gotgbot.Bot, ctx *ext.Context) error {
 	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word or try me inline by just writing my username in text box to search in cleanpng.com!\nBy @Memers_Gallery!\nSource code: https://github.com/annihilatorrrr/cleanpngbot",
-		&gotgbot.SendMessageOpts{DisableWebPagePreview: true})
+		&gotgbot.SendMessageOpts{
+			DisableWebPagePreview: true,
+			ReplyMarkup: &gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{{
+					SwitchInlineQuery: "",
+				}},
+			}},
+		})
 	return ext.EndGroups
 }
 
