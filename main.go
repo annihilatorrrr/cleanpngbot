fa5ade280f3b77fe71cdf1de4546da5666d24fb6
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-27 01:33:47 +0530

    Done. (#7)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/callbackquery"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/inlinequery"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
@@ -18,32 +19,39 @@ import (
 )
 
 func start(b *gotgbot.Bot, ctx *ext.Context) error {
-	search := ""
-	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word or try me inline by just writing my username in text box to search in cleanpng.com!\nBy @Memers_Gallery!\nSource code: https://github.com/annihilatorrrr/cleanpngbot",
+	query := ""
+	_, _ = ctx.EffectiveMessage.Reply(b, "I'm alive, send me a word or try me inline by just writing my username in text box or send /search command followed by the query to search in cleanpng.com!\nBy @Memers_Gallery!\nSource code: https://github.com/annihilatorrrr/cleanpngbot",
 		&gotgbot.SendMessageOpts{
 			DisableWebPagePreview: true,
 			ReplyMarkup: &gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
 				{{
 					Text:              "Try me inline!",
-					SwitchInlineQuery: &search,
+					SwitchInlineQuery: &query,
 				}},
 			}},
 		})
 	return ext.EndGroups
 }
 
-func procequery(rquery string) string {
+func procequery(rquery, page string) string {
 	query := strings.ToLower(rquery)
 	if strings.Contains(query, " ") {
 		query = strings.Join(strings.Split(query, " "), "-")
 	}
-	raw, err := soup.Get(fmt.Sprintf("https://www.cleanpng.com/free/%s.html", query))
+	srchstr := "https://www.cleanpng.com/free/%s.html"
+	txt := fmt.Sprintf("<b>Here's the search results for %s with thier resolutions and disk sizes:</b>", query)
+	if page != "0" {
+		srchstr = "https://www.cleanpng.com/free/%s" + fmt.Sprintf(",%s", page) + ".html"
+		txt += fmt.Sprintf("\n<b>Page: %s</b>\n\n", page)
+	} else {
+		txt += "\n\n"
+	}
+	raw, err := soup.Get(fmt.Sprintf(srchstr, query))
 	if err != nil {
-		return err.Error()
+		return "<b>No data Found!<b>\n" + err.Error()
 	}
 	datas := soup.HTMLParse(raw).FindAll("article")
 	aa := false
-	txt := fmt.Sprintf("<b>Here's the search results for %s with thier resolutions and disk sizes:</b>\n\n", query)
 	for _, rdata := range datas {
 		pd := rdata.FindAll("p")
 		if len(pd) < 3 {
@@ -59,9 +67,61 @@ func procequery(rquery string) string {
 	}
 	txt += "\n<b>@Memers_Gallery</b>"
 	if !aa {
-		txt = "No data Found!\n<b>@Memers_Gallery</b>"
+		txt = "<b>No data Found!\n@Memers_Gallery</b>"
 	}
 	return txt
+}
+
+func callbackhand(b *gotgbot.Bot, ctx *ext.Context) error {
+	query := ctx.CallbackQuery
+	splited := strings.Split(query.Data, "=")
+	data := splited[1]
+	page := splited[2]
+	txt := procequery(data, page)
+	if strings.Contains(txt, "<b>No data Found!") {
+		_, _, _ = query.Message.EditText(b, txt, &gotgbot.EditMessageTextOpts{ParseMode: "html"})
+		return ext.EndGroups
+	}
+	intpage, _ := strconv.Atoi(page)
+	backint := intpage - 1
+	if backint < 1 {
+		backint = 0
+	}
+	var err error
+	if backint == 0 {
+		_, _, err = query.Message.EditText(b, txt, &gotgbot.EditMessageTextOpts{
+			DisableWebPagePreview: true,
+			ParseMode:             "html",
+			ReplyMarkup: gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{{
+					Text:         "Next Page >",
+					CallbackData: fmt.Sprintf("call=%s=%d", data, intpage+1),
+				}},
+			}},
+		})
+	} else {
+		_, _, err = query.Message.EditText(b, txt, &gotgbot.EditMessageTextOpts{
+			DisableWebPagePreview: true,
+			ParseMode:             "html",
+			ReplyMarkup: gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{
+					{
+						Text:         "< Previous Page",
+						CallbackData: fmt.Sprintf("call=%s=%d", data, backint),
+					},
+					{
+						Text:         "Next Page >",
+						CallbackData: fmt.Sprintf("call=%s=%d", data, intpage+1),
+					},
+				},
+			}},
+		})
+	}
+	if err != nil {
+		_, _, _ = query.Message.EditText(b, err.Error(), nil)
+	}
+	_, _ = query.Answer(b, nil)
+	return ext.EndGroups
 }
 
 func sendres(b *gotgbot.Bot, ctx *ext.Context) error {
@@ -77,8 +137,21 @@ func sendres(b *gotgbot.Bot, ctx *ext.Context) error {
 	if err != nil {
 		return ext.EndGroups
 	}
-	txt := procequery(msg.Text)
-	_, _, err = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{DisableWebPagePreview: true, ParseMode: "html"})
+	txt := procequery(msg.Text, "0")
+	if strings.Contains(txt, "<b>No data Found!") {
+		_, _, _ = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{ParseMode: "html"})
+		return ext.EndGroups
+	}
+	_, _, err = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{
+		DisableWebPagePreview: true,
+		ParseMode:             "html",
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+			{{
+				Text:         "Next Page >",
+				CallbackData: fmt.Sprintf("call=%s=2", msg.Text),
+			}},
+		}},
+	})
 	if err != nil {
 		_, _, _ = em.EditText(b, err.Error(), nil)
 	}
@@ -109,15 +182,56 @@ func sendinline(b *gotgbot.Bot, ctx *ext.Context) error {
 		}, nil)
 		return ext.EndGroups
 	}
-	txt := procequery(q.Query)
+	txt := procequery(q.Query, "0")
 	_, _ = q.Answer(b, []gotgbot.InlineQueryResult{
 		gotgbot.InlineQueryResultArticle{
-			Id:                  uuid.NewString(),
-			Title:               "Results!",
-			Description:         "Found something!",
-			InputMessageContent: gotgbot.InputTextMessageContent{MessageText: txt, ParseMode: "html", DisableWebPagePreview: true},
+			Id:          uuid.NewString(),
+			Title:       "Results!",
+			Description: "Found something!",
+			InputMessageContent: gotgbot.InputTextMessageContent{
+				MessageText:           txt,
+				ParseMode:             "html",
+				DisableWebPagePreview: true,
+			},
 		},
 	}, nil)
+	return ext.EndGroups
+}
+
+func search(b *gotgbot.Bot, ctx *ext.Context) error {
+	msg := ctx.EffectiveMessage
+	args := ctx.Args()[1:]
+	if len(args) == 0 {
+		_, _ = msg.Reply(b, "I need a query to search!", nil)
+		return ext.EndGroups
+	}
+	query := args[0]
+	if len(query) > 50 {
+		_, _ = msg.Reply(b, "Query is too big to search!", nil)
+		return ext.EndGroups
+	}
+	em, err := msg.Reply(b, "Finding ...", nil)
+	if err != nil {
+		return ext.EndGroups
+	}
+	txt := procequery(query, "0")
+	if strings.Contains(txt, "<b>No data Found!") {
+		_, _, _ = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{ParseMode: "html"})
+		return ext.EndGroups
+	}
+	_, _, err = em.EditText(b, txt, &gotgbot.EditMessageTextOpts{
+		DisableWebPagePreview: true,
+		ParseMode:             "html",
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+			{{
+				Text:         "Next Page >",
+				CallbackData: fmt.Sprintf("call=%s=2", query),
+			}},
+		}},
+	})
+	if err != nil {
+		_, _, _ = em.EditText(b, err.Error(), nil)
+	}
 	return ext.EndGroups
 }
 
@@ -151,12 +265,14 @@ func main() {
 
 	dispatcher := updater.Dispatcher
 	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewCommand("search", search))
 	dispatcher.AddHandler(handlers.NewInlineQuery(inlinequery.All, sendinline))
+	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Prefix("call="), callbackhand))
 	dispatcher.AddHandler(handlers.NewMessage(message.ChatType("private"), sendres))
 
 	_, err = b.SetWebhook(url, &gotgbot.SetWebhookOpts{
 		DropPendingUpdates: true,
-		AllowedUpdates:     []string{"message", "inline_query", "chosen_inline_result"},
+		AllowedUpdates:     []string{"message", "inline_query", "chosen_inline_result", "callback_query"},
 		MaxConnections:     20,
 		SecretToken:        "xyzzz",
 	})
